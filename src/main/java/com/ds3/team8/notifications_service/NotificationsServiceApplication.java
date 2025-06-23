f8fa04355458afaf73554a8a91dd718c8e010f9b
JuanLopezAranzazu <juanlopezaranzazu@gmail.com>
2025-06-22 23:24:13 -0500

    Estructura inicial del microservicio

---
+++ src/main/java/com/ds3/team8/notifications_service/NotificationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ds3.team8.notifications_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationsServiceApplication.class, args);
+	}
+
+}
