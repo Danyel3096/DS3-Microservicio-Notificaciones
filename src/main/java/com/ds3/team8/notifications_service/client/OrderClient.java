e9b9d2504e4d0b712818a133a1c358005ee90cad
Victor Alzate <victoralzate61@gmail.com>
2025-06-23 22:21:59 -0500

    Feat: Implement Feign clients for Order and User services with response DTOs

---
+++ src/main/java/com/ds3/team8/notifications_service/client/OrderClient.java
@@ -0,0 +1,15 @@
+package com.ds3.team8.notifications_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.ds3.team8.notifications_service.client.dtos.OrderResponse;
+import com.ds3.team8.notifications_service.config.FeignClientInterceptor;
+
+@FeignClient(name = "orders-service", configuration = FeignClientInterceptor.class)
+public interface OrderClient {
+    
+    @GetMapping("/api/v1/orders/{id}")
+    OrderResponse getOrderById(@PathVariable("id") Long id);
+}
---
+++ src/main/java/com/ds3/team8/notifications_service/client/enums/Role.java
@@ -0,0 +1,7 @@
+package com.ds3.team8.notifications_service.client.enums;
+
+public enum Role {
+    ADMIN,
+    CUSTOMER,
+    DRIVER
+}
